d2c53e5a393e84143df1fd5f0ce3c682c94506f4
김성욱 <daniel@i-screamedu.co.kr>
2024-07-29 13:44:30 +0900

    기능2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("main");
-        System.out.println("test11");
-        System.out.println("test12");
-        System.out.println("test3");
-        System.out.println("test4");
+        System.out.println("기능1");
+        System.out.println("기능2");
     }
 }
