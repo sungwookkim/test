512753ba55988911d1812fc9c73c852dd336cb17
김성욱 <daniel@i-screamedu.co.kr>
2024-07-29 15:22:57 +0900

    Revert "Revert "기능 클래스 4""
    This reverts commit 55d077a8

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,6 @@ public class Main {
         System.out.println("test3");
         System.out.println("test4");
         System.out.println("test5");
-        System.out.println("test6");
-        System.out.println("test7");
     }
 
     public static void 기능1() {}
