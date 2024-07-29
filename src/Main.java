decd7a6daee387b46ed68326a0d2e343934d98bf
김성욱 <daniel@i-screamedu.co.kr>
2024-07-29 12:53:59 +0900

    1-n

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("main");
-        System.out.println("test1");
+        System.out.println("test12");
         System.out.println("test2");
         System.out.println("test3");
         System.out.println("test4");
