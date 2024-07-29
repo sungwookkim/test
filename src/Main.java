adadb0104289fb55f863c2887d8ce3e1cbae461b
김성욱 <daniel@i-screamedu.co.kr>
2024-07-29 15:23:26 +0900

    Reapply "기능 클래스 4"
    This reverts commit 55d077a83d1f78acb5a227bfdcec1cdbf29f6484.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println("test3");
         System.out.println("test4");
         System.out.println("test5");
+        System.out.println("test6");
     }
 
     public static void 기능1() {}
