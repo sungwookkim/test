1fdb1a12c09a9fe356ae20aa3b7df659ac111b7c
김성욱 <daniel@i-screamedu.co.kr>
2024-07-29 13:58:03 +0900

    Revert "no message"
    This reverts commit 874c862e

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("main");
-        System.out.println("기능1");
-        System.out.println("기능2");
-        System.out.println("기능3");
+        System.out.println("test1");
+        System.out.println("test2");
+        System.out.println("test3");
+        System.out.println("test4");
     }
 }
